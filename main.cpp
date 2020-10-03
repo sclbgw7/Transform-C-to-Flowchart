28fb83bf9a70d87d03c8426e13888c799302a800
sclbgw7 <sclbgw7@gmail.com>
2020-10-03 22:20:45 +0800

    add code.cpp and main,cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,29 @@
 
 using namespace std;
 
+//reserved words
+string rword[19]=
+{"","short","int","float","double","char",//0-5
+"if","else","while","for","return",//6-10
+"break","continue","define","include","getchar",//11-15
+"scanf","printf"};//16-17
+
 int main()
 {
 
+	//freopen("code.cpp","r",stdin);
+	//freopen("temp_1.dot","w",stdout);
+	//read
+
+	//puts("digraph gr1{a;b;c;d;test_yyhyyds;aa->b;b->d;c->d;}");
+
+	//write
+
+
+	//freopen("CON","r",stdin);
+	//freopen("CON","w",stdout);
+	system(".\\Graphviz_2.44.1\\bin\\dot -Tpng temp_1.dot -o Flowchart.png");
+	puts("Finished.");
+	system("pause");
 	return 0;
 }
