d42e79b1adfebc6f94c02de93460b679f98f0cd1
sclbgw7 <sclbgw7@gmail.com>
2020-10-05 22:51:42 +0800

    update main.cpp and .exe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include <cstdlib>/*tyty
-*/
+#include <cstdlib>
 #include <cstdio>
 #include <cstring>
 #include <string>
@@ -12,9 +11,40 @@ string rword[19]=
 {"","short","int","float","double","char",//0-5
 "if","else","while","for","return",//6-10
 "break","continue","define","include","getchar",//11-15
-"scanf","printf"};//16-17
+"scanf","printf","main"};//16-18
 
 map<string,int>vars,defs;//int determines type(1-5)
+//static var
+char ch;string str;
+
+void getchara()
+{
+	ch=getchar();
+	while(ch!=EOF&&(ch==' '||ch=='\n'||ch=='\t'))
+		ch=getchar();
+}
+
+void getword()
+{
+	str.clear();
+	getchara();
+	while(ch!=EOF&&((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')))
+	{
+		str.push_back(ch);
+		ch=getchar();
+	}
+}
+
+int apply_num()
+{
+	static int num=0;
+	return ++num;
+}
+
+int handlefunc(int lastnum);
+int handleif(int lastnum);
+int handlewhile(int lastnum);
+int handlefor(int lastnum);
 
 int main()
 {
@@ -22,7 +52,7 @@ int main()
 	freopen("code.c","r",stdin);
 	freopen("code_temp.c","w",stdout);//ATENTION:should be code_temp0.c
 
-	char ch=getchar();
+	ch=getchar();
 	while(ch!=EOF)
 	{
 		if(ch=='"')
@@ -35,8 +65,11 @@ int main()
 		{
 			ch=getchar();
 			if(ch=='/')
+			{
 				do ch=getchar(); 
 				while(ch!='\n'&&ch!=EOF);
+				if(ch=='\n')putchar(ch);
+			}
 			else if(ch=='*')
 			{
 				while(ch!=EOF)
@@ -56,7 +89,7 @@ int main()
 		ch=getchar();
 	}
 
-	/* handle define
+	/* handle define and include
 	freopen("code_temp0.c","r",stdin);
 	freopen("code_temp.c","w",stdout);
 
@@ -65,6 +98,14 @@ int main()
 
 	freopen("code_temp.c","r",stdin);
 	freopen("temp_1.dot","w",stdout);
+
+	int nownum;
+	printf("digraph main{\na%d[label=\"Begin\" shape=\"oval\"]\n",nownum=apply_num());
+
+
+	int lastnum=/*handlefunc()*/nownum;
+	printf("a%d[label=\"End\" shape=\"oval\"]\n",nownum=apply_num());
+	printf("a%d->a%d\n}",lastnum,nownum);
 
 	freopen("CON","r",stdin);
 	freopen("CON","w",stdout);
